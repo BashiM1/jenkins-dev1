1ddb20ec782d2ce224532989742ca2746ba761f7
BashiM1 <mbas2097152@outlook.com>
2026-04-02 13:25:33 +0100

    added public bucket access to test-bucket.tf

--- test-bucket.tf
+++ test-bucket.tf
@@ -39,6 +39,33 @@ resource "aws_s3_object" "submission_evidence" {
   source_hash  = filemd5("${path.module}/evidence/${each.key}")
 }
 
+resource "aws_s3_bucket_public_access_block" "assets_access" {
+  bucket = "jenkins-gcheck-assets"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "assets_read_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.assets_access]
+  bucket     = "jenkins-gcheck-assets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::jenkins-gcheck-assets/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "jenkins-bucket-"
   force_destroy = true
